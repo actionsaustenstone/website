fa9328c0f0717e0099ea107c67924d13ac6613de
Austen Stone <austenstone@github.com>
2023-11-02 11:49:17 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { fibonacci } from '@actionsaustenstone/math-package/src/index.js'
 
 const number = document.getElementById('number');
-const value = number.value;
-
-const result = fibonacci(value);
-
 const resultElement = document.getElementById('result');
-resultElement.innerHTML = `The fibonacci of ${value} is ${result}`;
+number.addEventListener('change', () => {
+  const value = number.value;
+  const result = fibonacci(value);
+  resultElement.innerHTML = `The fibonacci of ${value} is ${result}`;
+});
