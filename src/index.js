cd27128ded20cfa0a4f844b23121db93a5e22a3e
Austen Stone <austenstone@github.com>
2023-11-02 11:54:04 -0400

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ number.addEventListener('change', () => {
   const value = number.value;
   const result = fibonacci(value);
   if (!isNaN(value)) {
-    resultElement.innerHTML = `The fibonacci of ${value} is ${result}`;
+    resultElement.innerHTML = `The fibonacci of ${value} is ${result.join(', ')}`;
   } else {
     resultElement.innerHTML = 'Please enter a number';
   }
